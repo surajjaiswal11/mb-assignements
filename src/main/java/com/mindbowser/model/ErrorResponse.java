9e70820fd41ceee34b545211f718a01b714e4dfd
surajjaiswal11 <suraj.jaiswal@mindbowser.com>
2022-09-02 13:44:53 +0530

    first commit

--- src/main/java/com/mindbowser/model/ErrorResponse.java
+++ src/main/java/com/mindbowser/model/ErrorResponse.java
@@ -3,37 +3,18 @@ package com.mindbowser.model;
 import java.util.Date;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
+
 	private Date timestamp;
 	private String message;
 	private List<String> details;
 	private int statusCode;
-	
-
-	public ErrorResponse(Date timestamp, String message, List<String> details, int statusCode) {
-		super();
-		this.timestamp = timestamp;
-		this.message = message;
-		this.details = details;
-		this.statusCode = statusCode;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public List<String> getDetails() {
-		return details;
-	}
-	
 
 }
